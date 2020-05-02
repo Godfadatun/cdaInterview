24e9845724f7ec656b16084c20aaf74ec3b16dbb
Godfadatun <danyadegoke@gmail.com>
2020-05-02 06:11:50 -0400

    Add google site Tag

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -29,14 +29,15 @@
         <link href="{{ asset('assets/css/style.css')}}" rel="stylesheet">
 
         <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-165321862-1"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-165353516-1"></script>
         <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-        gtag('config', 'UA-165321862-1');
+            gtag('config', 'UA-165353516-1');
         </script>
+
 
 
         </head>
